c352dc47107af1ace2097ac2f6eaecbc8266f8bb
Duygu Kaya <118531388+Duyguukaya@users.noreply.github.com>
2024-12-30 15:12:41 +0300

    firebase ile bağlantı kuruldu

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'package:tedarify/girisSayfa.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Firebase için gerekli
-  await Firebase.initializeApp(); // Firebase'i başlatır
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(); 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
